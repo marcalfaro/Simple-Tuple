f5a399164503b6e79d15290ab6ad10dc042e96b0
Marc <marcalfaro@live.com>
2019-08-02 23:04:03 +0800

    Add project files.

---
+++ Simple Tuple/Module1.vb
@@ -0,0 +1,20 @@
+﻿Option Explicit On
+Module Module1
+
+    Sub Main()
+
+    End Sub
+
+    Private Function Add_Subtract_Multiply_Divide(ByVal num1 As Double, ByVal num2 As Double) As Tuple(Of Boolean, Double, String)
+        Dim ok As Boolean = False
+        Dim rslt As Double = 0.0
+        Dim err As String = String.Empty
+        Try
+
+        Catch ex As Exception
+
+        End Try
+
+    End Function
+
+End Module
